f771fef58db7e6e4ed5641083696d3c8b2825e8d
Nicco Ryan <niccoryan0@gmail.com>
2020-07-16 17:22:37 -0700

    Finished all methods

--- Lab09-LINQ/Program.cs
+++ Lab09-LINQ/Program.cs
@@ -16,6 +16,11 @@ namespace Lab09_LINQ
         static void Main(string[] args)
         {
             GetJson();
+            OutputAll();
+            IEnumerable<Features> noNoNames = FilterNoNameNeighborhoods(JsonData);
+            OnlyDistinctNeighborhoods(noNoNames);
+            ConsolidatedMethods();
+            FilterNoNamesWithQueryStatements();
         }
 
 
@@ -23,7 +28,6 @@ namespace Lab09_LINQ
         {
             string jsonString = File.ReadAllText("../../../../data.json");
             JsonData = JsonConvert.DeserializeObject<Root>(jsonString);
-            
         }
 
         public static void OutputAll()
@@ -37,11 +41,59 @@ namespace Lab09_LINQ
             Console.Clear();
         }
 
-        public static void FilterNoNameNeighborhoods()
+        public static IEnumerable<Features> FilterNoNameNeighborhoods(Root data)
         {
-            Root filteredData = JsonData.features.Select(x => x.properties.neighborhood != "");
+            IEnumerable<Features> filteredData = data.features.Where(x => x.properties.neighborhood != "");
+            foreach (var item in filteredData)
+            {
+                Console.WriteLine(item.properties.neighborhood);
+            }
+            Console.WriteLine($"Total: {filteredData.Count()}, press any key to continue.");
+            Console.ReadLine();
+            Console.Clear();
+            return filteredData;
         }
 
+        public static IEnumerable<string> OnlyDistinctNeighborhoods(IEnumerable<Features> data)
+        {
+            IEnumerable<string> filteredData = data.Select(x => x.properties.neighborhood).Distinct();
+            foreach (var item in filteredData)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total: {filteredData.Count()}, press any key to continue(Next screen will be same items, but LINQ queries are consolidated).");
+            Console.ReadLine();
+            Console.Clear();
+            return filteredData;
+        }
+
+        public static void ConsolidatedMethods()
+        {
+            IEnumerable<string> data = JsonData.features.Where(x => x.properties.neighborhood != "")
+                                                        .Select(x => x.properties.neighborhood)
+                                                        .Distinct();
+            foreach (var item in data)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"Total: {data.Count()}, press any key to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public static void FilterNoNamesWithQueryStatements()
+        {
+            IEnumerable<Features> data = from location in JsonData.features
+                                           where location.properties.neighborhood != ""
+                                           select location;
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.properties.neighborhood);
+            }
+            Console.WriteLine($"Total: {data.Count()}, press any key to exit the program.");
+            Console.ReadLine();
+            Console.Clear();
+        }
 
     }
 
