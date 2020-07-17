94536e42175908c7159d978a5daba21ab61ad746
niccoryan0 <niccoryan0@gmail.com>
2020-07-17 15:38:41 -0700

    Added summary comments

--- Lab09-LINQ/Program.cs
+++ Lab09-LINQ/Program.cs
@@ -12,8 +12,10 @@ namespace Lab09_LINQ
     {
         public static Root JsonData { get; set; }
 
-
-        static void Main(string[] args)
+        /// <summary>
+        /// Runs each method to output the information for the lab.
+        /// </summary>
+        static void Main()
         {
             GetJson();
             OutputAll();
@@ -23,13 +25,18 @@ namespace Lab09_LINQ
             FilterNoNamesWithQueryStatements();
         }
 
-
+        /// <summary>
+        /// Deserialize JSON data
+        /// </summary>
         public static void GetJson()
         {
             string jsonString = File.ReadAllText("../../../../data.json");
             JsonData = JsonConvert.DeserializeObject<Root>(jsonString);
         }
 
+        /// <summary>
+        /// Output all the neighborhood names from the JSON data
+        /// </summary>
         public static void OutputAll()
         {
             foreach (var item in JsonData.features)
@@ -41,6 +48,11 @@ namespace Lab09_LINQ
             Console.Clear();
         }
 
+        /// <summary>
+        /// Outputs all neighborhood names that aren't empty
+        /// </summary>
+        /// <param name="data">Data to be queried</param>
+        /// <returns>Filtered data</returns>
         public static IEnumerable<Features> FilterNoNameNeighborhoods(Root data)
         {
             IEnumerable<Features> filteredData = data.features.Where(x => x.properties.neighborhood != "");
@@ -54,6 +66,11 @@ namespace Lab09_LINQ
             return filteredData;
         }
 
+        /// <summary>
+        /// Filters the neighborhood data to only unique neighborhood names
+        /// </summary>
+        /// <param name="data">Data to be filtered</param>
+        /// <returns>Filtered data</returns>
         public static IEnumerable<string> OnlyDistinctNeighborhoods(IEnumerable<Features> data)
         {
             IEnumerable<string> filteredData = data.Select(x => x.properties.neighborhood).Distinct();
@@ -67,6 +84,9 @@ namespace Lab09_LINQ
             return filteredData;
         }
 
+        /// <summary>
+        /// Filters data down to non-empty unique neighborhood names in one string of methods
+        /// </summary>
         public static void ConsolidatedMethods()
         {
             IEnumerable<string> data = JsonData.features.Where(x => x.properties.neighborhood != "")
@@ -81,6 +101,9 @@ namespace Lab09_LINQ
             Console.Clear();
         }
 
+        /// <summary>
+        /// Filter out unnamed neighborhoods using a query statement
+        /// </summary>
         public static void FilterNoNamesWithQueryStatements()
         {
             IEnumerable<Features> data = from location in JsonData.features
